abec102a129cf0368b2eb2239ab68d8132cb9a02
Andreas Burger <andreas.burger@mail.utoronto.ca>
2024-05-13 16:35:52 -0400

    launchrun

--- multiruns/e2_layer_scaling.sh
+++ multiruns/e2_layer_scaling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# replace `run` with `sbatch scripts/amd_launcher.slrm train_deq_md_v2.py` or `sbatch scripts/slurm_launcher.slrm train_deq_md_v2.py`
+# replace `launchrun` with `sbatch scripts/amd_launcher.slrm train_deq_md_v2.py` or `sbatch scripts/slurm_launcher.slrm train_deq_md_v2.py`
 
 md17=(aspirin benzene ethanol malonaldehyde naphtalene salicyclic_acid toluene uracil)
 md22=(AT_AT_CG_CG AT_AT Ac_Ala3_NHMe DHA buckyball_catcher dw_nanotube stachyose)
@@ -8,5 +8,5 @@ md22=(AT_AT_CG_CG AT_AT Ac_Ala3_NHMe DHA buckyball_catcher dw_nanotube stachyose
 layers=(1 2 3 4 5 6 7 8)
 
 for l in ${layers[@]}; do
-  run model.num_layers=$l 
+  launchrun model.num_layers=$l 
 done
--- multiruns/md17.sh
+++ multiruns/md17.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-# replace `run` with `sbatch scripts/amd_launcher.slrm train_deq_md_v2.py` or `sbatch scripts/slurm_launcher.slrm train_deq_md_v2.py`
+# replace `launchrun` with `sbatch scripts/amd_launcher.slrm train_deq_md_v2.py` or `sbatch scripts/slurm_launcher.slrm train_deq_md_v2.py`
+
+# run with `bash multiruns/md17.sh`
 
 md17=(aspirin benzene ethanol malonaldehyde naphtalene salicyclic_acid toluene uracil)
 md22=(AT_AT_CG_CG AT_AT Ac_Ala3_NHMe DHA buckyball_catcher dw_nanotube stachyose)
 
 for mol in ${md17[@]}; do
-  run +use=deq +cfg=fpc_of target=$mol
-#   run model.num_layers=1 target=$mol
-#   run model.num_layers=4 target=$mol
+  launchrun+use=deq +cfg=fpc_of target=$mol
+#   launchrunmodel.num_layers=1 target=$mol
+#   launchrunmodel.num_layers=4 target=$mol
 done
 
 # Baseline Equiformer2 with one layer
-# run model.num_layers=1 target=aspirin
-# run model.num_layers=1 target=benzene
-# run model.num_layers=1 target=ethanol
-# run model.num_layers=1 target=malonaldehyde
+# launchrunmodel.num_layers=1 target=aspirin
+# launchrunmodel.num_layers=1 target=benzene
+# launchrunmodel.num_layers=1 target=ethanol
+# launchrunmodel.num_layers=1 target=malonaldehyde
 
-# run model.num_layers=1 target=naphtalene
-# run model.num_layers=1 target=salicyclic_acid
-# run model.num_layers=1 target=toluene
-# run model.num_layers=1 target=uracil
+# launchrunmodel.num_layers=1 target=naphtalene
+# launchrunmodel.num_layers=1 target=salicyclic_acid
+# launchrunmodel.num_layers=1 target=toluene
+# launchrunmodel.num_layers=1 target=uracil
 
 
 # Baseline Equiformer2 with four layers
-# run model.num_layers=4 target=aspirin
-# run model.num_layers=4 target=benzene
-# run model.num_layers=4 target=ethanol
-# run model.num_layers=4 target=malonaldehyde
+# launchrunmodel.num_layers=4 target=aspirin
+# launchrunmodel.num_layers=4 target=benzene
+# launchrunmodel.num_layers=4 target=ethanol
+# launchrunmodel.num_layers=4 target=malonaldehyde
 
-# run model.num_layers=4 target=naphtalene
-# run model.num_layers=4 target=salicyclic_acid
-# run model.num_layers=4 target=toluene
-# run model.num_layers=4 target=uracil
+# launchrunmodel.num_layers=4 target=naphtalene
+# launchrunmodel.num_layers=4 target=salicyclic_acid
+# launchrunmodel.num_layers=4 target=toluene
+# launchrunmodel.num_layers=4 target=uracil

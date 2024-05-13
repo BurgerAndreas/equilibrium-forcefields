7b08348080a94e5b4a52ed6c1821aca6647b3e75
Andreas Burger <andreas.burger@mail.utoronto.ca>
2024-05-13 16:39:22 -0400

    launchrun

--- multiruns/md17.sh
+++ multiruns/md17.sh
@@ -8,30 +8,30 @@ md17=(aspirin benzene ethanol malonaldehyde naphtalene salicyclic_acid toluene u
 md22=(AT_AT_CG_CG AT_AT Ac_Ala3_NHMe DHA buckyball_catcher dw_nanotube stachyose)
 
 for mol in ${md17[@]}; do
-  launchrun+use=deq +cfg=fpc_of target=$mol
-#   launchrunmodel.num_layers=1 target=$mol
-#   launchrunmodel.num_layers=4 target=$mol
+  launchrun +use=deq +cfg=fpc_of target=$mol
+#   launchrun model.num_layers=1 target=$mol
+#   launchrun model.num_layers=4 target=$mol
 done
 
 # Baseline Equiformer2 with one layer
-# launchrunmodel.num_layers=1 target=aspirin
-# launchrunmodel.num_layers=1 target=benzene
-# launchrunmodel.num_layers=1 target=ethanol
-# launchrunmodel.num_layers=1 target=malonaldehyde
+# launchrun model.num_layers=1 target=aspirin
+# launchrun model.num_layers=1 target=benzene
+# launchrun model.num_layers=1 target=ethanol
+# launchrun model.num_layers=1 target=malonaldehyde
 
-# launchrunmodel.num_layers=1 target=naphtalene
-# launchrunmodel.num_layers=1 target=salicyclic_acid
-# launchrunmodel.num_layers=1 target=toluene
-# launchrunmodel.num_layers=1 target=uracil
+# launchrun model.num_layers=1 target=naphtalene
+# launchrun model.num_layers=1 target=salicyclic_acid
+# launchrun model.num_layers=1 target=toluene
+# launchrun model.num_layers=1 target=uracil
 
 
 # Baseline Equiformer2 with four layers
-# launchrunmodel.num_layers=4 target=aspirin
-# launchrunmodel.num_layers=4 target=benzene
-# launchrunmodel.num_layers=4 target=ethanol
-# launchrunmodel.num_layers=4 target=malonaldehyde
+# launchrun model.num_layers=4 target=aspirin
+# launchrun model.num_layers=4 target=benzene
+# launchrun model.num_layers=4 target=ethanol
+# launchrun model.num_layers=4 target=malonaldehyde
 
-# launchrunmodel.num_layers=4 target=naphtalene
-# launchrunmodel.num_layers=4 target=salicyclic_acid
-# launchrunmodel.num_layers=4 target=toluene
-# launchrunmodel.num_layers=4 target=uracil
+# launchrun model.num_layers=4 target=naphtalene
+# launchrun model.num_layers=4 target=salicyclic_acid
+# launchrun model.num_layers=4 target=toluene
+# launchrun model.num_layers=4 target=uracil

6b19a4f35df86312c16774f4adf5d432bc84e102
Andreas Burger <andreas.burger@mail.utoronto.ca>
2024-07-27 03:13:16 -0400

    deq runs

---
+++ multiruns/md17_deq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# replace `launchrun` with `sbatch scripts/amd_launcher.slrm train_deq_md_v2.py` or `sbatch scripts/slurm_launcher.slrm train_deq_md_v2.py`
+
+# run with `source multiruns/md17.sh`
+
+md17=(aspirin benzene ethanol malonaldehyde naphthalene salicylic_acid toluene uracil)
+md22=(AT_AT_CG_CG AT_AT Ac_Ala3_NHMe DHA buckyball_catcher dw_nanotube stachyose)
+
+for mol in ${md17[@]}; do
+    launchrun +use=deq +cfg=bp wandb_tags=["md17"] target=$mol
+    # launchrun model.num_layers=1 +cfg=dd wandb_tags=["md17"] target=$mol
+    # launchrun model.num_layers=4 +cfg=dd  wandb_tags=["md17"] target=$mol
+    # launchrun model.num_layers=8 +cfg=dd  wandb_tags=["md17"] target=$mol
+done

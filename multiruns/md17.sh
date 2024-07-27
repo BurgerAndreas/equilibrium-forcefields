a275e9c541c75a137f32c9958aeb03b7a20f047c
Andreas Burger <andreas.burger@mail.utoronto.ca>
2024-07-27 03:10:40 -0400

    paper config

--- multiruns/md17.sh
+++ multiruns/md17.sh
@@ -2,7 +2,7 @@
 
 # replace `launchrun` with `sbatch scripts/amd_launcher.slrm train_deq_md_v2.py` or `sbatch scripts/slurm_launcher.slrm train_deq_md_v2.py`
 
-# run with `bash multiruns/md17.sh`
+# run with `source multiruns/md17.sh`
 
 md17=(aspirin benzene ethanol malonaldehyde naphthalene salicylic_acid toluene uracil)
 md22=(AT_AT_CG_CG AT_AT Ac_Ala3_NHMe DHA buckyball_catcher dw_nanotube stachyose)

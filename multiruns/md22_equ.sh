680add3de8e68368281790ee87a292502caf75e5
Andreas Burger <andreas.burger@mail.utoronto.ca>
2024-07-27 03:40:22 -0400

    md22 multiruns

--- multiruns/md22_equ.sh
+++ multiruns/md22_equ.sh
@@ -2,16 +2,16 @@
 
 # replace `launchrun` with `sbatch scripts/amd_launcher.slrm train_deq_md_v2.py` or `sbatch scripts/slurm_launcher.slrm train_deq_md_v2.py`
 
-# run with `source multiruns/md17.sh`
+# run with `source multiruns/md22_equ.sh`
 
 md17=(aspirin benzene ethanol malonaldehyde naphthalene salicylic_acid toluene uracil)
 md22=(AT_AT_CG_CG AT_AT Ac_Ala3_NHMe DHA buckyball_catcher dw_nanotube stachyose)
 
 for mol in ${md22[@]}; do
-    # launchrun +use=deq +cfg=fpc_of wandb_tags=["md17"] target=$mol
-    launchrun model.num_layers=1 +cfg=dd wandb_tags=["md17"] target=$mol
-    launchrun model.num_layers=4 +cfg=dd  wandb_tags=["md17"] target=$mol
-    launchrun model.num_layers=8 +cfg=dd  wandb_tags=["md17"] target=$mol
+    # launchrun +use=deq +cfg=fpc_of wandb_tags=["md22"] target=$mol
+    launchrun model.num_layers=1 +cfg=dd wandb_tags=["md22"] dname=md22 target=$mol
+    launchrun model.num_layers=4 +cfg=dd  wandb_tags=["md22"] dname=md22 target=$mol
+    launchrun model.num_layers=8 +cfg=dd  wandb_tags=["md22"] dname=md22 target=$mol
 done
 
 

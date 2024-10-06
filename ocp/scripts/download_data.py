845a3558845bf3ab4b8dde43572ac95d3fd1aa99
Andreas Burger <andreas.burger@mail.utoronto.ca>
2024-10-06 09:27:57 -0400

    download only lmdb

--- ocp/scripts/download_data.py
+++ ocp/scripts/download_data.py
@@ -43,7 +43,7 @@ S2EF_COUNTS = {
 }
 
 
-def get_data(datadir, task, split, del_intmd_files):
+def get_data(datadir, task, split, del_intmd_files, download_only=False):
     os.makedirs(datadir, exist_ok=True)
 
     if task == "s2ef" and split is None:
@@ -59,7 +59,14 @@ def get_data(datadir, task, split, del_intmd_files):
     elif task == "is2re":
         download_link = DOWNLOAD_LINKS[task]
 
+    # download data
+    logging.info(f"Downloading {task}...")
     os.system(f"wget {download_link} -P {datadir}")
+
+    if download_only:
+        return
+
+    # extract data and preprocess
     filename = os.path.join(datadir, os.path.basename(download_link))
     logging.info("Extracting contents...")
     os.system(f"tar -xvf {filename} -C {datadir}")
@@ -161,6 +168,11 @@ if __name__ == "__main__":
         default=os.path.join(os.path.dirname(ocpmodels.__path__[0]), "data"),
         help="Specify path to save dataset. Defaults to 'ocpmodels/data'",
     )
+    parser.add_argument(
+        "--download-only",
+        action="store_true",
+        help="Download data only, do not preprocess",
+    )
 
     args, _ = parser.parse_known_args()
     get_data(
@@ -168,4 +180,5 @@ if __name__ == "__main__":
         task=args.task,
         split=args.split,
         del_intmd_files=not args.keep,
+        download_only=args.download_only,
     )
